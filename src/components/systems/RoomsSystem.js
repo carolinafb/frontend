dc7f8e86014752b0fb13e58bbea721984002b063
Juan Massucco <juanmassucco@gmail.com>
2020-11-06 21:03:14 -0300

    system modificado

--- src/components/systems/RoomsSystem.js
+++ src/components/systems/RoomsSystem.js
@@ -1,42 +1,54 @@
 import React from "react";
-import { Table, Space, Collapse } from "antd";
+import { Table, Space, Collapse, Button } from "antd";
 const { Panel } = Collapse;
 
 const RoomsSystem = ({ rooms }) => {
+  const columns = [
+    {
+      title: "Cama",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Paciente",
+      render: (text, record) => (
+
+        <Space size="middle">
+           {record["patient_last_name"]} {record["patient_name"]}{record["patient_name"] ? <p></p>: <p>Vacia</p>}
+        </Space>
+      ),
+      key: "patient_last_name",
+    },
+    {
+      title: "Acciones",
+      key: "Acciones",
+      render: (text, record) => (
+        <Space size="middle">
+          {record["patient_id"] ? <Button type="primary">Ver</Button>: <a/>}
+          {record["patient_id"] ? <Button type="primary">Evolucionar</Button>: <a/>
+        }
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <div>
       <Collapse accordion>
         {rooms &&
           rooms.map((room, index) => (
             <Panel header={room.name} key={`room${index}`}>
-              <Table dataSource={room.beds} pagination={false}>
-                <Table title="Bed Name" dataIndex="name" key="firstName" />
-                <Table
-                  title="name"
-                  dataIndex="patient_name"
-                  key="patient_name"
-                />
-
-                <Table
-                  title="lastname"
-                  dataIndex="patient_last_name"
-                  key="patient_last_name"
-                />
-                <Table
-                  title="Action"
-                  key="action"
-                  render={(text, record) => (
-                    <Space size="middle">
-                      <a>Invite</a>
-                      <a>Delete</a>
-                    </Space>
-                  )}
-                />
-              </Table>
+              <Table
+                dataSource={room.beds}
+                pagination={false}
+                columns={columns}
+                scroll={{ x: 700 }}
+              ></Table>
             </Panel>
           ))}
       </Collapse>
     </div>
   );
 };
+
 export default RoomsSystem;
