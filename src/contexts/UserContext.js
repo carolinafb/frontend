d31b420b057f1609843bd1a30b99c77fe5066c51
Luciano Pérez Cerra <luchopcerra@gmail.com>
2020-11-18 14:45:36 -0300

    [guardo-el-user-en-contexto] guardo el user en el contexto

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -5,10 +5,11 @@ export const UserContext = createContext();
 //provider
 const UserProvider = (props) => {
   const [jwt, setJwt] = useState(null);
+  const [DBUser, setDBUser] = useState(null);
   const apiEndPoint = "https://localhost:9000";
 
   return (
-    <UserContext.Provider value={{ jwt, setJwt, apiEndPoint }}>
+    <UserContext.Provider value={{ jwt, setJwt, apiEndPoint, setDBUser, DBUser }}>
       {props.children}
     </UserContext.Provider>
   );
