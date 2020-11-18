d5a8333d3c74b5fc563f971f58e46b9efe822d64
Luciano Pérez Cerra <luchopcerra@gmail.com>
2020-11-18 20:36:06 -0300

    [guardo-el-user-en-contexto] salvar el caso del server

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -2,24 +2,27 @@ import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
+
 //provider
 const UserProvider = (props) => {
+  const IS_SERVER = typeof window === "undefined";
+  const HAS_STORAGE = (!IS_SERVER && window.sessionStorage);
   let defaultJWT = null;
   let defaultUser = null;
-  if (sessionStorage) {
+  if (HAS_STORAGE) {
     defaultJWT = JSON.parse(sessionStorage.getItem('jwt'));
     defaultUser = JSON.parse(sessionStorage.getItem('user'));
   };
   const [jwt, _setJwt] = useState(defaultJWT);
   const setJwt = (data) => {
-    if (sessionStorage) {
+    if (HAS_STORAGE) {
       sessionStorage.setItem('jwt', JSON.stringify(data));
     };
     _setJwt(data);
   };
   const [DBUser, _setDBUser] = useState(defaultUser);
   const setDBUser = (data) => {
-    if (sessionStorage) {
+    if (HAS_STORAGE) {
       sessionStorage.setItem('user', JSON.stringify(data));
     };
     _setDBUser(data);
