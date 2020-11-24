c16178e3f1ce76f0114fe5056ca89125a6d0413f
Carolina Fernandez <carolinafernandezbeltran@gmail.com>
2020-11-24 20:15:16 -0300

    consulta inicial por camas libres para crear internacion

--- src/pages/internment/create/index.js
+++ src/pages/internment/create/index.js
@@ -9,82 +9,43 @@ import {
   Form,
   DatePicker,
   Input,
-  Select,
-  Spin,
 } from "antd";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, Fragment } from "react";
 import { useRouter } from "next/router";
-import axiosInstance from "../../../components/axios";
+//import axiosInstance from "../../components/axios";
 import { UserContext } from "../../../contexts/Context";
 
 const CreateInternment = () => {
   const router = useRouter();
   const { Header, Content } = Layout;
   const { TextArea } = Input;
-  const [infoRooms, setInfoRooms] = useState(null);
-  const [infoBeds, setInfoBeds] = useState(null);
+  const [createInternmentData, setcreateInternmentData] = useState(null);
   const [err, setErr] = useState(false);
-  const [loading, setLoading] = useState(false);
   const { Title } = Typography;
-  const { DBUser, needCreateBeds, idPatient } = useContext(UserContext);
-  const { Option } = Select;
+  const { DBUser } = useContext(UserContext);
+  const [rerender, setRerender] = useState(true);
 
   const btnErr = () => {
-    setLoading(true);
     if (DBUser && DBUser.role == "DOCTOR") {
       router.push("/patients");
     } else {
       router.push("/systems");
     }
   };
-  function callToBackToAddData(url, values) {
-    setLoading(true);
+
+  useEffect(() => {
     axiosInstance
-      .put(url, values)
+      .get("/system")
       .then((res) => {
-        if (res.status) {
-          router.push(res.data.redirect);
-        }
-      })
-      .catch((err) => {
-        setErr(err.message);
-      });
-  }
-  const callToBackForInfo = (method, url, param) => {
-    setLoading(true);
-    axiosInstance
-      .request({ method, url, params: param })
-      .then((res) => {
-        url === "/beds/withSpace" && setInfoBeds(res.data);
-        if (res.data.length === 0) {
-          callToBackForInfo("get", "/system", { id: 1 });
-        }
-        setInfoRooms(res.data);
-        setLoading(false);
+        setErr(false);
+        setData(res.data);
       })
       .catch((e) => {
         setErr(e.message);
+        setData(null);
       });
-  };
-
-  const onFinish = (values) => {
-    setLoading(true);
-    values.idPatient = idPatient;
-    if (!needCreateBeds) {
-      callToBackToAddData("/internment", values);
-    } else {
-      callToBackToAddData("/internmentWithNewBed", values);
-    }
-  };
-
-  const onRoomSelect = (room) => {
-    if (!needCreateBeds) {
-      callToBackForInfo("get", "/beds/withSpace", { id: room });
-    }
-  };
-  useEffect(() => {
-    callToBackForInfo("get", "/rooms/withSpace", { id: 1 });
-  }, []);
+  }),
+    [];
 
   return (
     <Layout>
@@ -117,22 +78,24 @@ const CreateInternment = () => {
               <Title align="center" level={3} style={{ marginTop: "3%" }}>
                 CREAR INTERNACION
               </Title>
-              {loading && (
-                <div className="align-column-center margin__big">
-                  <Spin size="large" tip="Loading..." />
-                </div>
+              {err && (
+                <Alert
+                  message={err}
+                  type="error"
+                  style={{ alignContent: "center" }}
+                />
               )}
+
               <Form
                 layout="vertical"
                 name="createInternment"
-                onFinish={onFinish}
                 initialValues={{
                   remember: true,
                 }}
               >
                 <Form.Item
                   label="Fecha de inicio de sintomas:"
-                  name="dateOfSymptoms"
+                  name="DateSymptom "
                   rules={[
                     {
                       required: true,
@@ -145,7 +108,7 @@ const CreateInternment = () => {
 
                 <Form.Item
                   label="Fecha de inicio diagnostico:"
-                  name="dateOfDiagnosis"
+                  name="DateDiagnosticStart"
                   rules={[
                     {
                       required: true,
@@ -156,79 +119,8 @@ const CreateInternment = () => {
                   <DatePicker placeholder="Fecha de inicio diagnostico" />
                 </Form.Item>
 
-                <Form.Item
-                  label="Comorbilidades: "
-                  name="historyOfDisease"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Campo obligatorio!",
-                    },
-                  ]}
-                >
+                <Form.Item label="Comorbilidades: " name="comorbidities">
                   <TextArea placeholder="Leve descripcion" />
-                </Form.Item>
-                {infoRooms && (
-                  <Form.Item
-                    label="Salas: "
-                    name="room"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Por favor ingrese la sala!",
-                      },
-                    ]}
-                  >
-                    <Select onSelect={onRoomSelect}>
-                      {infoRooms.map((element) => {
-                        return (
-                          <Option value={element.id} key={element.id}>
-                            {element.name}
-                          </Option>
-                        );
-                      })}
-                    </Select>
-                  </Form.Item>
-                )}
-                {needCreateBeds ? (
-                  <Form.Item
-                    label="Nombre de la cama:"
-                    name="bed"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Por favor ingrese el nombre!",
-                      },
-                    ]}
-                  >
-                    <Input placeholder="Nombre de la cama" />
-                  </Form.Item>
-                ) : (
-                  infoBeds && (
-                    <Form.Item
-                      label="Camas libres: "
-                      name="bed"
-                      rules={[
-                        {
-                          required: true,
-                          message: "Por favor ingrese la cama!",
-                        },
-                      ]}
-                    >
-                      <Select>
-                        {infoBeds.map((element) => {
-                          return (
-                            <Option key={element.id}>{element.name}</Option>
-                          );
-                        })}
-                      </Select>
-                    </Form.Item>
-                  )
-                )}
-                <Form.Item>
-                  <Button type="primary" htmlType="submit">
-                    CREAR INTERNACION
-                  </Button>
                 </Form.Item>
               </Form>
             </Col>
