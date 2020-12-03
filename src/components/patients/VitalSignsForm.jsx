0556a57e121899abd13702f8b632d3357d436f56
Luciano Pérez Cerra <luchopcerra@gmail.com>
2020-12-02 23:25:40 -0300

    agregué la página para evolucionar paciente

---
+++ src/components/patients/VitalSignsForm.jsx
@@ -0,0 +1,105 @@
+import React, { useRef } from "react";
+import { Form, Input } from "antd";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+const VitalSignsForm = ({ form }) => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      form={form}
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Temperatura"
+        name="temperature"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingresa la temperatura!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="TA sistólica"
+        name="tasistolic"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingresa la TA sistólica!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="TA diastolica"
+        name="tadistolic"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingresa la TA diastolica!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="FC"
+        name="fc"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingresa la FC!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="FR"
+        name="fr"
+        rules={[
+          {
+            required: true,
+            message: "Por favor ingresa la FR!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default VitalSignsForm;
---
+++ src/pages/patient/[id]/evolve.jsx
@@ -0,0 +1,109 @@
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "src/contexts/Context";
+import Navbar from "src/components/header/Navbar";
+import { Layout, Steps, Button, Form } from "antd";
+const { Step } = Steps;
+import Head from "next/head";
+import axiosInstance from "src/components/axios";
+import { useRouter } from "next/router";
+import VitalSignsForm from "src/components/patients/VitalSignsForm";
+
+const Evolve = ({ ...props }) => {
+  const router = useRouter();
+  const { DBUser } = useContext(UserContext);
+  const { Header, Content } = Layout;
+  const [patientName, setPatientName] = useState();
+  const [current, setCurrent] = React.useState(0);
+  const [form] = Form.useForm();
+
+  const finishHandler = () => {
+    form.validateFields().then((data) => {
+      axiosInstance
+        .post("/patient/evolve", { ...data })
+        .then(() => router.push("/patients"))
+        .catch((err) => console.log(err));
+    });
+  };
+
+  const steps = [
+    {
+      title: "Signos vitales",
+      content: <VitalSignsForm form={form} />,
+    },
+    {
+      title: "Sistema respiratorio",
+      content: "Second-content",
+    },
+    {
+      title: "Otros síntomas",
+      content: "Last-content",
+    },
+    {
+      title: "Estudios de hoy",
+      content: "Last-content",
+    },
+    {
+      title: "Observaciones",
+      content: "Last-content",
+    },
+  ];
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  useEffect(() => {
+    // console.log(router.query.id);
+    const patientID = router.query.id;
+    if (!patientID) return;
+    axiosInstance
+      .get("/patient", { params: { id: patientID } })
+      .then((response) =>
+        setPatientName(`${response.data.name}, ${response.data.lastName}`)
+      );
+    const patientName = "Carlos"; // await axiosInstance.get('/patient');
+    setPatientName(patientName);
+  }, [router.query.id]);
+  return (
+    <>
+      <Head>
+        <title>Evolucionar paciente</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Layout>
+        <Header style={{ backgroundColor: "rgb(107, 45, 177)" }}>
+          <Navbar user={DBUser} />
+        </Header>
+        <Content>
+          <h1>{patientName}</h1>
+        </Content>
+        <Steps size="small" current={current}>
+          {steps.map((item) => (
+            <Step key={item.title} title={item.title} />
+          ))}
+        </Steps>
+        <div className="steps-content">{steps[current].content}</div>
+        <div className="steps-action">
+          {current < steps.length - 1 && (
+            <Button type="primary" onClick={() => next()}>
+              Siguiente
+            </Button>
+          )}
+          <Button type="primary" onClick={finishHandler}>
+            Finalizar
+          </Button>
+          {current > 0 && (
+            <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
+              Anterior
+            </Button>
+          )}
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Evolve;
