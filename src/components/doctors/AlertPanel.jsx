70664022c2e4228bfdf3a4e26c0607c900051f92
Luciano Pérez Cerra <luchopcerra@gmail.com>
2020-12-15 15:10:02 -0300

    agregué que marque la alerta como vista

--- src/components/doctors/AlertPanel.jsx
+++ src/components/doctors/AlertPanel.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Alert } from "antd";
 
-const onClose = (e) => {
-  console.log(e, "I was closed.");
+const onClose = (alertID) => {
+  axiosInstance.put("/alert-seen", {
+    id: alertID,
+  });
+  console.log("marcar como vista Alert ID => ", alertID);
+  console.log("I was closed.");
 };
 
 const AlertPanel = ({ alerts }) => {
@@ -14,7 +18,7 @@ const AlertPanel = ({ alerts }) => {
           message={alert.message}
           type="warning"
           closable
-          onClose={onClose}
+          onClose={() => onClose(alert.id)}
         />
       ))}
     </>
