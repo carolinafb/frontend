43745aea60f69ee211a1b4f368822b2169629f28
Juan Massucco <juanmassucco@gmail.com>
2020-12-15 20:28:55 -0300

    prueba 2

--- src/contexts/Context.js
+++ src/contexts/Context.js
@@ -2,19 +2,20 @@ import React, { createContext, useEffect, useState } from "react";
 const UserContext = createContext();
 
 const UserProvider = (props) => {
-  //console.log("se llamos al use provider");
+  console.log("se llamos al use provider");
   let IS_SERVER = typeof window === "undefined";
   const HAS_STORAGE = !IS_SERVER && window.sessionStorage;
   let defaultUser = null;
 
   if (HAS_STORAGE) {
     defaultUser = JSON.parse(sessionStorage.getItem("user"));
+    console.log("se ejecuta el user provider", defaultUser);
   }
   const [DBUser, _setDBUser] = useState(defaultUser);
   const setDBUser = (data) => {
-    //console.log("se llama al setDBUSER");
+    console.log("se llama al setDBUSER");
     if (HAS_STORAGE) {
-      //console.log("entro en el setDBUser con HAS Storage");
+      console.log("entro en el setDBUser con HAS Storage");
       sessionStorage.setItem("user", JSON.stringify(data));
     }
     _setDBUser(data);
